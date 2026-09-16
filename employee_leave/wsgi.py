14671496d84e157d5b80297cb815f317a7015539
pvijaikr <vijaikri001@e.ntu.edu.sg>
2026-09-16 17:27:53 +0530

    Auto migrate

--- employee_leave/wsgi.py
+++ employee_leave/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'employee_leave.settings')
 
 application = get_wsgi_application()
+
+# Automatically run database migrations on server startup so tables exist on Render
+try:
+    call_command('migrate', interactive=False)
+except Exception as e:
+    print(f"Auto-migration warning: {e}")
